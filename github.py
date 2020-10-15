883e8f8a2c0fb8ff3d6702e3c993055effcbb088
OmidBakhshaei <omidb67@gmail.com>
2020-10-15 16:23:07 +0330

    Making sure that all the inputs are numbers

--- github.py
+++ github.py
@@ -1,7 +1,10 @@
 def Sum(*args):
     All = 0
     for arg in args:
-        All += arg
+        if isinstance(arg, float) or isinstance(arg, int):
+            All += arg
+        else:
+            raise ValueError("One or more inputs are not numbers!")
     return All
 
 print (Sum(7, 9, 4))
